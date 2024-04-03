bee317d2d41d9913e3012e48c86f78eca269c08f
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2024-04-03 13:17:47 +0200

    Finished Course

---
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
+
+	FString Name = GetName();
+
+	UE_LOG(LogTemp, Display, TEXT("BeginPlay: %s"), *Name);
+}
+
+void AMovingPlatform::Unpause()
+{
+	isPaused = false;
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (!isPaused)
+	{
+		MovePlatform(DeltaTime);
+	}
+	RotatePlatform(DeltaTime);
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	if (ShouldPlatformReturn())
+	{
+		isPaused = true;
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		StartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(StartLocation);
+		FTimerHandle UniqueHandle;
+		FTimerDelegate RespawnDelegate = FTimerDelegate::CreateUObject(this, &AMovingPlatform::Unpause);
+		GetWorldTimerManager().SetTimer(UniqueHandle, RespawnDelegate, 2, false);
+		PlatformVelocity = -PlatformVelocity;
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+		SetActorLocation(CurrentLocation);
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	FRotator CurrentRotation = GetActorRotation();
+	CurrentRotation = CurrentRotation + RotationVelocity * DeltaTime;
+	SetActorRotation(CurrentRotation);
+}
+
+bool AMovingPlatform::ShouldPlatformReturn() const
+{
+	return GetDistanceMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved() const
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
